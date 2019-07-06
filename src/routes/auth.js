715459713f1959e341b68708ac7a0c957c45fe32
Nzubechukwu Nnamani <nzubechukwunnamani@Nzubechukwus-MacBook-Pro.local>
2019-07-06 14:13:15 +0100

    chore(fix swagger documentation bug): fix swagger bug [Start #167116306]

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,36 @@ import AuthController from '../controllers/Auth';
 
 const router = express.Router();
 
+/**
+* @swagger
+*
+* /auth/google:
+*   get:
+*     tags:
+*       - auth
+*     description: Google Social Login
+*     produces:
+*       - application/json
+*     responses:
+*       201:
+*         description: Success
+*/
 router.post('/login', AuthController.login);
+
+/**
+* @swagger
+*
+* /auth/google:
+*   post:
+*     tags:
+*       - auth
+*     description: Google Social Login
+*     produces:
+*       - application/json
+*     responses:
+*       201:
+*         description: Success
+*/
 router.post('/logout', AuthController.logOut);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -3,41 +3,22 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import cors from 'cors';
 import errorhandler from 'errorhandler';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import config from './db/config/config';
 import router from './routes/index';
+import swaggerSpec from '../swagger';
+
 
 const { isProduction, port } = config;
 
 // Create global app object
 const app = express();
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Authors Haven',
-      version: '1.0.0',
-      description: 'Endpoints for Authors Haven'
-    },
-    securityDefinitions: {
-      bearerAuth: {
-        type: 'apiKey',
-        name: 'Authorization',
-        scheme: 'bearer',
-        in: 'header',
-      },
-    },
-  },
-  apis: ['./routes/api/*.js'],
-};
-const swaggerSpec = swaggerJSDoc(options);
-
-app.get('/api-docs.json', (req, res) => {
+app.get('/api/v1/doc', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
 });
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
