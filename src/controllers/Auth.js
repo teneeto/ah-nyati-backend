bcbb4d26c348503c902def543f53fb6371ed7d3a
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2019-07-06 04:55:02 +0100

    feat(Auth):User signup endpoint
    Integrate user signup route
    Integrate Nodemailer
    User should receive a mail to confirm account registration
    
    [Delivers #166841006]

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,20 +1,71 @@
-import auth from '../middleware/Auth';
+import passwordHash from 'password-hash';
+import Auth from '../helpers/helpers';
 import { Blacklist, User } from '../db/models';
+import Template from '../helpers/mail/MailTemplate';
+import Mailer from '../helpers/mail/Mailer';
+
+const { generateToken } = Auth;
+const { sendMail } = Mailer;
+
 /**
  *
  *
  * @class AuthController
  */
 class AuthController {
-/**
- *
- *
- * @static
- * @param {object} req
- * @param {object} res
- * @returns {object} res
- * @memberof AuthController
- */
+  /**
+  *
+  * @constructor
+  * @description signup a user
+  * @static
+  * @param {object} req
+  * @param {object} res
+  * @memberof AuthController
+  */
+  static async createAccount(req, res) {
+    try {
+      const { userName, email, password } = req.body;
+      const hashedpassword = passwordHash.generate(password);
+      const values = { userName, email, password: hashedpassword };
+      const result = await User.create(values);
+      const { id } = result;
+      const token = await generateToken({ id, userName, email });
+
+      const user = {
+        id: result.id,
+        userName: result.userName,
+        email: result.email,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt
+      };
+
+      const url = `${req.protocol}://${req.get('host')}/api/v1/auth/signup/${token}`;
+      const message = Template.resetEmail(url);
+      const subject = 'Welcome to Authors Haven';
+      await sendMail({ to: email, subject, html: message });
+
+      return res.status(201).json({
+        status: 201,
+        message: 'Signup succesfull, a comfirmation link has been sent to your mailbox',
+        data: [{ user }],
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+      });
+    }
+  }
+
+  /**
+*
+*
+* @static
+* @param {object} req
+* @param {object} res
+* @returns {object} res
+* @memberof AuthController
+*/
   static async login(req, res) {
     const { email } = req.body;
     try {
@@ -23,7 +74,7 @@ class AuthController {
         attributes: { exclude: ['password'] }
       });
       const { id } = user;
-      const userToken = auth.authenticate(id);
+      const userToken = Auth.authenticate(id);
       if (user) {
         return res.status(200).json({
           status: 200,
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ import AuthController from '../controllers/Auth';
 
 const router = express.Router();
 
+router.post('/signup', AuthController.createAccount);
 router.post('/login', AuthController.login);
 router.post('/logout', AuthController.logOut);
 
